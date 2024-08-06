e71866eaff09bc7844ed9b2e75c8d6777592255d
rlfhrdyd <ysh01022321@gmail.com>
2024-08-06 18:00:59 +0900

    first commit

---
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -0,0 +1,12 @@
+package com.mysite.sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.mysite.sbb.question.QuestionService;
+
+@SpringBootTest
+class SbbApplicationTests {
+
+}
